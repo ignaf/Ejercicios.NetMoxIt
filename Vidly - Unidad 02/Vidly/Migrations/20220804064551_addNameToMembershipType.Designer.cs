6dcd4245a428f0328ff129291bd91d1bcad0ea1d
ignaf <ignaa88@gmail.com>
2022-08-04 04:00:36 -0300

    Unidad 03 - Ejercicio 01

--- Vidly - Unidad 02/Vidly/Migrations/20220804064551_addNameToMembershipType.Designer.cs
+++ Vidly - Unidad 02/Vidly/Migrations/20220804064551_addNameToMembershipType.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Vidly.DAL;
 
 namespace Vidly.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220804064551_addNameToMembershipType")]
+    partial class addNameToMembershipType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
