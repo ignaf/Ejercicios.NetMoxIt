a9fc2ca3c4e0f80d2e1581cef7a3c4c1ef3be22f
ignaf <ignaa88@gmail.com>
2022-08-05 22:03:41 -0300

    facebook login

--- Vidly - Unidad 02/Vidly/Controllers/UsersController.cs
+++ Vidly - Unidad 02/Vidly/Controllers/UsersController.cs
@@ -5,6 +5,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Facebook;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -205,6 +206,33 @@ namespace Vidly.Controllers
             }
             ViewBag.Msg = "Wrong email or password";
             return View();
+        }
+
+        [Route("users/facebook-login")]
+        public IActionResult FacebookLogin()
+        {
+            var properties = new AuthenticationProperties
+            {
+                RedirectUri = Url.Action("FacebookResponse")
+            };
+
+            return Challenge(properties, FacebookDefaults.AuthenticationScheme);
+        }
+
+        [Route("users/facebook-response")]
+        public async Task<IActionResult> FacebookResponse()
+        {
+            var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var claims = result.Principal.Identities
+                .FirstOrDefault().Claims.Select(claim => new
+                {
+                    claim.Issuer,
+                    claim.OriginalIssuer,
+                    claim.Type,
+                    claim.Value
+                });
+            return Json(claims);
         }
 
         public async Task<IActionResult> Logout()
